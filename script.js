db8a5012c7f80b66ceef6b9d2136218c558bb54f
Pavel <210937@unsaac.edu.pe>
2025-05-03 11:31:42 -0500

    Plantilla base

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("form-contacto").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Gracias por contactarnos.");
+  });
+  
+  document.getElementById("form-registro").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Emprendimiento registrado correctamente.");
+  });
+  
